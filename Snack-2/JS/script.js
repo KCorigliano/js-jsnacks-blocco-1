12934be9e3f4413eb81de374065dc6cff0132707
KCorigliano <corigliano98@hotmail.com>
2021-12-15 13:05:20 +0100

    Snack-2

---
+++ Snack-2/JS/script.js
@@ -0,0 +1,9 @@
+let yourNum = [];
+yourNum = parseInt(prompt('Inserisci qua il tuo numero'));
+let num=0;
+
+for (i=0; i<=yourNum; i++){
+    let square = (i*i);
+    console.log(`${i}: ${square}`);
+}
+
