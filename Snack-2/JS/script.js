3d7feca2dd9a7fada9fe5b33ec9a1efa143efc8f
KCorigliano <corigliano98@hotmail.com>
2021-12-16 09:47:39 +0100

    fix linee inutili cancellate

--- Snack-2/JS/script.js
+++ Snack-2/JS/script.js
@@ -1,9 +1,7 @@
-let yourNum = [];
-yourNum = parseInt(prompt('Inserisci qua il tuo numero'));
-let num=0;
+let yourNum = parseInt(prompt('Inserisci qua il tuo numero'));
 
 for (i=0; i<=yourNum; i++){
-    let square = (i*3);
-    console.log(`${i}: ${square}`);
+    let cube = (i*3);
+    console.log(`${i}: ${cube}`);
 }
 
